8dd05afb24c82bc05c0e1582b6d89d29d6926651
Viggo Rogestam <viggo.rogestam@protonmail.com>
2024-01-09 14:09:53 +0100

    pseudo code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,3 +5,16 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+// class main
+    // public static void
+
+    //loop that runs the program
+
+    //if-statments to choose which converter to use (area, volumes etc.)
+
+    //when choosen run a loop inside the loop that runs an imported method with the code to run the calculation
+
+    //print result and ask user if they want to run the method again or
+    //go back to the main loop and choose another calculation
+
