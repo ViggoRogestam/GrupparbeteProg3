e977bcfeb69a644a644db757afcf114fe07381a1
LinaRomilson <lina@romilson>
2024-01-10 16:13:15 +0100

    english + symbols

--- src/main/java/org/example/Area.java
+++ src/main/java/org/example/Area.java
@@ -16,10 +16,10 @@ public class Area {
         System.out.print("..:: Area converter ::.." + """
                                 
                 ------------------------
-                1. Circle
-                2. Rectangle
-                3. Triangle
-                4. Liksidig hexagon
+                1. Circle \t\t\t\t| \u25CB
+                2. Rectangle \t\t\t| \u25AD
+                3. Triangle \t\t\t| \u25B3
+                4. Equilateral hexagon \t| \u2B21
                 ------------------------
                 Choose a shape:""" + " ");
         int shape = scan.nextInt();
@@ -32,56 +32,57 @@ public class Area {
                 // 2. Cirkel
                 if (shape == 1) {
                     // 2.1 Ange radien
-                    System.out.print("Ange radien: ");
-                    double radie = scan.nextDouble();
+                    System.out.print("Enter the radius: ");
+                    double radius = scan.nextDouble();
                     // 2.1.1 Använd metoden circle
-                    double area = circle(radie);
+                    double area = circle(radius);
                     // 2.2 Visa resultatet
-                    System.out.printf("Cirkelns area är " + "%.2f\n", area);
+                    System.out.printf("The area of the circle is " + "%.2f\n", area);
                     // 2.3 Ändra tryAgain till false
                     tryAgain = false;
                 }
                 // 3. Rektangel
                 else if (shape == 2) {
                     // 3.1 Ange sidornas längd
-                    System.out.print("Ange bredden: ");
+                    System.out.print("Enter the width: ");
                     double b = scan.nextDouble();
-                    System.out.print("Ange höjden: ");
+                    System.out.print("Enter the height: ");
                     double h = scan.nextDouble();
                     // 3.1.1 Använd metoden rectangle
                     double area = rectangle(h,b);
                     // 3.2 Visa resultatet
-                    System.out.printf("Rektangelns area är " + "%.2f\n", area);
+                    System.out.printf("The area of the rectangle is " + "%.2f\n", area);
                     // 3.3 Ändra tryAgain till false
                     tryAgain = false;
                 }
                 // 4. Triangel
                 else if (shape == 3) {
                     // 4.1 Ange höjd och bas
-                    System.out.print("Ange höjden: ");
+                    System.out.print("Enter the height: ");
                     double h = scan.nextDouble();
-                    System.out.print("Ange basen: ");
+                    System.out.print("Enter the base: ");
                     double b = scan.nextDouble();
                     // 4.1.1 Använd metoden triangle
                     double area = triangle(h, b);
                     // 4.2 Visa resultatet
-                    System.out.printf("Triangelns area är " + "%.2f\n", area);
+                    System.out.printf("The area of the triangle is " + "%.2f\n", area);
                     // 4.3 Ändra tryAgain till false
                     tryAgain = false;
                 }
                 // 5. Hexagon
                 else {
                     // 5.1 Ange sidans längd
-                    System.out.print("Ange sidans längd: ");
+                    System.out.print("Enter the length of the side: ");
                     double s = scan.nextDouble();
                     // 5.1.1 Använd metoden hexagon
                     double area = hexagon(s);
                     // 5.2 Visa resultatet
-                    System.out.printf("Hexagonens area är " + "%.2f\n", area);
+                    System.out.printf("the area of the hexagon is " + "%.2f\n", area);
                     // 5.3 Ändra tryAgain till false
                     tryAgain = false;
                 }
-            } else {
+            }
+            else {
                 System.out.print("Fel: ange ett giltigt alternativ: ");
                 shape = scan.nextInt();
             }
