6438fb475fe6d58dc8ae8aa7a599f9d0fe325a70
Viggo Rogestam <viggo.rogestam@protonmail.com>
2024-01-12 15:05:10 +0100

    class TemperatureUnits.java v1.0 completed

---
+++ src/main/java/org/example/TemperatureUnits.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class TemperatureUnits {
+
+    public static double convTemp(double orgTemp, String fromUnit, String toUnit) {
+        // Convert units to uppercase and get the first character
+        char charToUnit = toUnit.toUpperCase().charAt(0);
+        char charFromUnit = fromUnit.toUpperCase().charAt(0);
+
+        // Convert original temperature to Celsius
+        double tempInCelsius;
+        switch (charFromUnit) {
+            case 'C':
+                tempInCelsius = orgTemp;
+                break;
+            case 'F':
+                tempInCelsius = (orgTemp - 32) * 5.0 / 9.0;
+                break;
+            case 'K':
+                tempInCelsius = orgTemp - 273.15;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid original unit: " + fromUnit);
+        }
+
+        // Convert from Celsius to the target unit
+        switch (charToUnit) {
+            case 'C':
+                return tempInCelsius;
+            case 'F':
+                return (tempInCelsius * 9.0 / 5.0) + 32;
+            case 'K':
+                return tempInCelsius + 273.15;
+            default:
+                throw new IllegalArgumentException("Invalid target unit: " + toUnit);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convTemp(32, "celcsius", "kelvin"));
+    }}
+
