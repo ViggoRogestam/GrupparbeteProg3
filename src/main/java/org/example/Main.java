1e710ab11241d63d4fb90ca1bef74b7b03746628
Viggo Rogestam <viggo.rogestam@protonmail.com>
2024-01-22 12:03:54 +0100

    base structure for main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,49 @@
 package org.example;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-
-
+        var scan = new Scanner(System.in);
+        int indata;
         while (true){
+            System.out.println("""
+                 ..:: Coordinate System ::..
+                
+                ------------------------------------------------------------
 
+                1. Calculate the length between two points
+                2. Find the midpoint between two points
+                3. Show the most recent midpoints (max 5 most recent)
+                
+                ------------------------------------------------------------""");
+            try {
+                indata = scan.nextInt();
+            }
+            catch (InputMismatchException e){
+                System.out.println("Error: Invalid input");
+                continue;
+            }
+            switch (indata){
+                case 1:
+                    continue;
+                case 2:
+                    continue;
+                case 3:
+                    continue;
+                case 4:
+                    continue;
+                case 5:
+                    continue;
+                case 6:
+                    continue;
+                case 7:
+                    continue;
+                case 8:
+                    continue;
+                default:
+                    System.out.println("Invalid input");
 
-
+            }
 
 
 
