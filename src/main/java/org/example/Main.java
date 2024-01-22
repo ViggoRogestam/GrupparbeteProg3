efd26cd58d38d3b224bee2c9ed3f41e40e2d8112
Viggo Rogestam <viggo.rogestam@protonmail.com>
2024-01-22 11:50:52 +0100

    class TemperatureUnits.java v1.0 completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+
+        while (true){
+
+
+
+
+
+
+
+
+
+        }
     }
 }
 
