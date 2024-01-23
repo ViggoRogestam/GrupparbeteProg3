90b72fdaeb286e61539ec2cd7660d5e29ce9a0e8
LinaRomilson <lina@romilson>
2024-01-23 13:25:33 +0100

    named outerloop + scanner as arg

--- src/main/java/org/example/Area.java
+++ src/main/java/org/example/Area.java
@@ -15,28 +15,26 @@ import java.util.*;
  */
 public class Area {
 
-    public static void main(String[] args) {
+    public static void main(String[] args, Scanner scan) {
 
         int shape = 0;
         // Kolla om inmatningen är giltig
-        try (Scanner scan = new Scanner(System.in)) {
-            // Loopa tills att ett giltigt val väljs
-            while (shape < 1 || shape > 4) {
-                printMenu();
+        // Loopa tills att ett giltigt val väljs
+        while (shape < 1 || shape > 4) {
+            printMenu();
 
-                while (!scan.hasNextInt()) {
-                    System.out.println("Error: Please enter a valid integer: ");
-                    scan.next(); // radera ogiltig inmatning
-                }
-                shape = scan.nextInt();
+            while (!scan.hasNextInt()) {
+                System.out.println("Error: Please enter a valid integer: ");
+                scan.next(); // radera ogiltig inmatning
+            }
+            shape = scan.nextInt();
 
-                // Visa felmeddelande om det valda numret är ogiltigt
-                if (shape < 1 || shape > 4) {
-                    System.out.println("Error: Please enter a valid option (1-4): ");
-                } else {
-                    // Beräkna arean på formen
-                    handleShapeInput(shape, scan);
-                }
+            // Visa felmeddelande om det valda numret är ogiltigt
+            if (shape < 1 || shape > 4) {
+                System.out.println("Error: Please enter a valid option (1-4): ");
+            } else {
+                // Beräkna arean på formen
+                handleShapeInput(shape, scan);
             }
         }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,94 +3,107 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        //int indata;
-        while (true) {
-            System.out.println("""
-                     . .:: Coordinate System ::..
-                                    
-                    ------------------------------------------------------------
-
-                    1. Calculate the length between two points
-                    2. Find the midpoint between two points
-                    3. Show the most recent midpoints (max 5 most recent)
-                                    
-                    ------------------------------------------------------------""");
-            int indata;
-            while (true) {
-                try {
-                    indata = scan.nextInt();
-                    break;
-                } catch (InputMismatchException e) {
-                    System.out.println("Error: Invalid input");
-                    scan.next(); // clear the scanner buffer
+        try (Scanner scan = new Scanner(System.in)) {
+            //int indata;
+            outerLoop: while (true) {
+                System.out.println("""
+                         . .:: Converter ::..
+                                        
+                        ------------------------------------------------------------
+                            
+                        1. Temperatures
+                        2. Area
+                        3. Coordinate system
+                                        
+                        ------------------------------------------------------------""");
+                int indata;
+                while (true) {
+                    try {
+                        indata = scan.nextInt();
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Error: Invalid input");
+                        scan.next(); // clear the scanner buffer
+                    }
                 }
-            }
-            switch (indata) {
-                case 1:
-                    while (true) {
-                        //declare variables
-                        double orgTemp;
-                        String fromUnit, toUnit, anwser;
-                        //ask user for input
-                        try {
-                            System.out.println("Enter temperature:");
-                            orgTemp = scan.nextDouble();
-                            System.out.println("Enter temperature unit (celcsius, farneheit or kelvin):");
-                            fromUnit = scan.next();
-                            System.out.println("Enter unit to convert to (celcsius, farneheit or kelvin)");
-                            toUnit = scan.next();
+                switch (indata) {
+                    case 1:
+                        while (true) {
+                            //declare variables
+                            double orgTemp;
+                            String fromUnit, toUnit, anwser;
+                            //ask user for input
+                            try {
+                                System.out.println("Enter temperature:");
+                                orgTemp = scan.nextDouble();
+                                System.out.println("Enter temperature unit (celcsius, farneheit or kelvin):");
+                                fromUnit = scan.next();
+                                System.out.println("Enter unit to convert to (celcsius, farneheit or kelvin)");
+                                toUnit = scan.next();
 
-                            //run convTemp class-method to convert temperature & print it out
-                            System.out.println(TemperatureUnits.convTemp(orgTemp, fromUnit, toUnit));
-                        } catch (Exception e) {
-                            System.out.println("Invalid input" + e);
-                            scan.nextLine(); // consume the next token, not the entire line
-                        } finally {
-                            System.out.println("\nDo you want to convert again (y/n)?");
-                            anwser = scan.next();
+                                //run convTemp class-method to convert temperature & print it out
+                                System.out.println(TemperatureUnits.convTemp(orgTemp, fromUnit, toUnit));
+                            } catch (Exception e) {
+                                System.out.println("Invalid input" + e);
+                                scan.nextLine(); // consume the next token, not the entire line
+                            } finally {
+                                System.out.println("\nDo you want to convert again (y/n)?");
+                                anwser = scan.next();
 
-                            //ask user if they want to continue the convertions or exit out of case 1
-                            if (!Objects.equals(anwser, "y"))
-                                break;
+                                //ask user if they want to continue the convertions or exit out of case 1
+                                if (!Objects.equals(anwser, "y"))
+                                    break;
+                            }
                         }
-                    }
 
 
-                case 2:
-                    Area.main(args);
-                    scan.nextLine();
-                    break;
-                case 3:
-                    while (true) {
-                        // Call the main method of the CoordinateSystem class
-                        CoordinateSystem.main(args);
-
-                        // Ask the user if they want to run the program again
-                        System.out.print("Do you want to run the program again? (y/n): ");
-                        String userInput = scan.next();
-
-                        // If the user enters anything other than 'y', exit the loop
-                        if (!userInput.equalsIgnoreCase("y")) {
-                            break;
+                    case 2:
+                        while (true) {
+                            // Call the main method of the Area class
+                            Area.main(args, scan);
+                            scan.nextLine();
+                            // Ask the user if they want to run the program again
+                            System.out.print("\nDo you want to run the program again? (y/n): ");
+                            String userInput = scan.nextLine();
+                            // If the user enters anything other than 'y', exit the loop
+                            if (!userInput.equalsIgnoreCase("y")) {
+                                continue outerLoop;
+                            }
                         }
-                    }
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                default:
-                    System.out.println("Invalid input");
+                    case 3:
+                        while (true) {
+                            // Call the main method of the CoordinateSystem class
+                            CoordinateSystem.main(args);
+
+                            // Ask the user if they want to run the program again
+                            System.out.print("Do you want to run the program again? (y/n): ");
+                            String input = scan.next();
+                            scan.nextLine();
+
+                            // If the user enters anything other than 'y', exit the loop
+                            if (!input.equalsIgnoreCase("y")) {
+                                scan.nextLine();
+                                continue outerLoop;
+
+                            }
+                        }
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+
+                }
+
 
             }
-
-
         }
     }
     // Method to read the user input
